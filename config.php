3675cf403e2efca12deee901487adaa408ebae0c
ivandomnich <domnich.ivan@gmail.com>
2016-10-06 12:21:58 +0300

    First commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/shop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/shop/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xamppp/htdocs/shop/catalog/');
+define('DIR_SYSTEM', 'C:/xamppp/htdocs/shop/system/');
+define('DIR_LANGUAGE', 'C:/xamppp/htdocs/shop/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xamppp/htdocs/shop/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xamppp/htdocs/shop/system/config/');
+define('DIR_IMAGE', 'C:/xamppp/htdocs/shop/image/');
+define('DIR_CACHE', 'C:/xamppp/htdocs/shop/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xamppp/htdocs/shop/system/storage/download/');
+define('DIR_LOGS', 'C:/xamppp/htdocs/shop/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xamppp/htdocs/shop/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xamppp/htdocs/shop/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'admin');
+define('DB_DATABASE', 'mystore');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
